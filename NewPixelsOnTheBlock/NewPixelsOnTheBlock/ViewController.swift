f8960acf2d7c0b411d5b8b15613114acc147fd25
Francesca Corsini <francesca.corsini@wuerth-phoenix.com>
2021-08-29 18:29:27 +0200

    Added project from the scratch

---
+++ NewPixelsOnTheBlock/NewPixelsOnTheBlock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewPixelsOnTheBlock
+//
+//  Created by Francesca Corsini on 29/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
