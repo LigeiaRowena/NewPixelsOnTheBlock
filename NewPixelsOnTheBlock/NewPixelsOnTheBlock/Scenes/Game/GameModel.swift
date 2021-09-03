053cdbae80df534450afd2d05160da02fb325ffa
Francesca Corsini <francesca.corsini@wuerth-phoenix.com>
2021-09-03 12:38:46 +0200

    Added proper model for game engine and unit tests

--- NewPixelsOnTheBlock/NewPixelsOnTheBlock/Scenes/Game/GameModel.swift
+++ NewPixelsOnTheBlock/NewPixelsOnTheBlock/Scenes/Game/GameModel.swift
@@ -7,6 +7,235 @@
 
 import Foundation
 
-class GameModel {
+enum Constants {
+    static let gameOverBlocksCount: Int = 10
+    static let notColouredBlocksScore: Int = 10
+    static let unitScore: Int = 5
+}
+
+/// Enum referred to the horizontal position (aka the row) of the single block within the entire matrix.
+/// The offset starts from the very top-left position of the matrix.
+enum X: Int, CaseIterable {
+    case one = 1
+    case two
+    case three
+    case four
+    case five
+}
+
+/// Enum referred to the vertical position (aka the column) of the single block within the entire matrix.
+/// The offset starts from the very top-left position of the matrix.
+enum Y: Int, CaseIterable {
+    case one = 1
+    case two
+    case three
+    case four
+    case five
+}
+
+/// Struct referred to the position of the block in the matrix in (x,y) coordinates
+struct Position: Equatable {
+    var x: X
+    var y: Y
+    
+    static func ==(lhs: Position, rhs: Position) -> Bool {
+        return lhs.x == rhs.x && lhs.y == rhs.y
+    }
+}
+
+/// Struct referred to the single block of the matrix.
+struct Block: Equatable {
+    var position: Position
+    var isColoured: Bool = false
+    var score: Int = 0
+}
+
+/// The model engine of Game scene. It's a custom collection.
+struct GameModel: Collection {
+    
+    //MARK: Collection properties and required functions
+ 
+    /// The array of data types used by the custom collection, it's a matrix of 25 blocks.
+    private var matrix: [Block]
+    
+    /// The position of the first element in the collection
+    var startIndex: Int {
+        return 0
+    }
+    
+    /// The position of the last element in the collection
+    var endIndex: Int {
+        return matrix.endIndex
+    }
+    
+    /// Returns the position immediately after the given index
+    /// - Parameter i: The given index
+    /// - Returns: The position just after the given index
+    func index(after i: Int) -> Int {
+        return matrix.index(after: i)
+    }
+    
+    init() {
+        self.matrix = [Block]()
+        X.allCases.forEach { x in
+            Y.allCases.forEach { y in
+                let block = Block(position: Position(x: x, y: y))
+                matrix.append(block)
+            }
+        }
+    }
+    
+    //MARK: Collection subscript functions
+    
+    /// Utility subscript function that uses the default index position
+    /// - Parameter index: The default index position of blocks array
+    subscript(index: Int) -> Block? {
+        get {
+            return index < endIndex ? matrix[index] : nil
+        }
+    }
+    
+    /// Utility subscript function that uses the position in (x,y) coordinates
+    /// - Parameter index: The position in (x,y) coordinates
+    subscript(position: Position) -> Block {
+        get {
+            guard let block = matrix.first(where: { $0.position == position }) else {
+                fatalError("The given Position doesn't return any valid Block element within matrix collection")
+            }
+            return block
+        }
+        set(newValue) {
+            guard let index = matrix.firstIndex(of: self[position]) else {
+                fatalError("The given Position doesn't return any valid Block element within matrix collection")
+            }
+            matrix.remove(at: index)
+            matrix.insert(newValue, at: index)
+        }
+    }
+    
+    //MARK: Utility functions
+        
+    /// Returns an array of only coloured Block elements
+    /// - Returns: An array of Block elements with isColoured = true
+    func colouredBlocks() -> [Block] {
+        return matrix.filter{ $0.isColoured == true }
+    }
+    
+    /// Returns an array of only not coloured Block elements for the entire matrix of blocks, each one located below a coloured Block element
+    /// According to the business logic of this game, a not coloured block below a coloured one has score = .ten
+    /// - Returns: An array of Block elements with isColoured = false and each one below a Block element with isColoured = true
+    func whiteBlocksWithScore() -> [Block] {
+        return matrix.filter{ $0.isColoured == false && isBelowAColouredBlock($0)}
+    }
+    
+    /// Returns true if a Block element is below a coloured one recursively, so even by skipping other not coloured blocks on the same Y position
+    /// - Parameter block: The given Block element
+    /// - Returns: True if a Block element is below a coloured one recursively
+    func isBelowAColouredBlock(_ block: Block) -> Bool {
+        guard let index = X.allCases.firstIndex(of: block.position.x) else {
+            fatalError("The given Block doesn't have a proper X position")
+        }
+        let availableXPositions = Array(X.allCases.suffix(from: index))
+        for x in availableXPositions {
+            // Edge case: the given Block element is located at the very top, so there's no other block above it
+            guard let nextX = X(rawValue: x.rawValue + 1) else {
+                return false
+            }
+            let anotherBlock = self[Position(x: nextX, y: block.position.y)]
+            if anotherBlock.isColoured {
+                return true
+            }
+        }
+        return false
+    }
+    
+    /// Returns true if a Block element is above a coloured one
+    /// - Parameter block: The given Block element
+    /// - Returns: A tuple with a flag=true if a Block element is above a coloured one, and the coloured block itself if any
+    func isAboveAColouredBlock(_ block: Block) -> (Bool, Block?) {
+        // Edge case: the given Block element is located at the very bottom, so there's no other block below it
+        guard let previousX = X(rawValue: block.position.x.rawValue - 1) else {
+            return (false, nil)
+        }
+        let anotherBlock = self[Position(x: previousX, y: block.position.y)]
+        let isAboveAColouredBlock = anotherBlock.isColoured
+        return (isAboveAColouredBlock, isAboveAColouredBlock ? anotherBlock : nil)
+    }
+    
+    /// Returns true if a Block element is above a not coloured one
+    /// - Parameter block: The given Block element
+    /// - Returns: True if a Block element is above a not coloured one
+    func isAboveANotColouredBlock(_ block: Block) -> Bool {
+        // Edge case: the given Block element is located at the very bottom, so there's no other block below it
+        guard let previousX = X(rawValue: block.position.x.rawValue - 1) else {
+            return false
+        }
+        let anotherBlock = self[Position(x: previousX, y: block.position.y)]
+        return !anotherBlock.isColoured
+    }
+    
+    /// Returns true if a Block element is between two coloured ones
+    /// - Parameter block: The given Block element
+    /// - Returns: True if a Block element is between two coloured ones
+    func isBetweenTwoColouredBLocks(_ block: Block) -> Bool {
+        // Edge case: the given Block element is located at the very right side or at the very left side, so the block is not between two coloured ones
+        guard let previousY = Y(rawValue: block.position.y.rawValue - 1),
+              let nextY = Y(rawValue: block.position.y.rawValue + 1) else {
+            return false
+        }
+        let previousBlock = self[Position(x: block.position.x, y: previousY)]
+        let nextBlock = self[Position(x: block.position.x, y: nextY)]
+        return previousBlock.isColoured && nextBlock.isColoured
+    }
+    
+    /// Sets a specific Block element with isColoured = true
+    /// - Parameter position: The position of the given Block element
+    /// - Returns: The just coloured Block element
+    mutating func tintBlock(at position: Position) -> Block {
+        var block = self[position]
+        block.isColoured = true
+        self[position] = block
+        return block
+    }
+
+    /// Returns true if there are at least 10 coloured blocks: in this case the game is over
+    /// - Returns: True if there are at least 10 coloured blocks
+    func isGameOver() -> Bool {
+        return colouredBlocks().count >= Constants.gameOverBlocksCount
+    }
+    
+    /// Returns the final score when the game is over
+    /// - Returns: The final score
+    mutating func finalScore() -> Int {
+        var score: Int = 0
+        
+        // First we calcolate the partial score from the coloured blocks
+        colouredBlocks().forEach { block in
+            var copy = block
+            switch block.position.x {
+            case .one:
+                copy.score += Constants.unitScore
+            case .two, .three, .four, .five:
+                if isAboveANotColouredBlock(copy) {
+                    copy.score += Constants.unitScore
+                } else if let colouredBlock = isAboveAColouredBlock(copy).1 {
+                    copy.score += colouredBlock.score + Constants.unitScore
+                }
+            }
+            self[block.position] = copy
+        }
+                
+        // Then we calculate and sum the partial score from the not coloured blocks
+        whiteBlocksWithScore().forEach { block in
+            var copy = block
+            copy.score = Constants.notColouredBlocksScore
+            self[block.position] = copy
+        }
+
+        let scores = matrix.map{ $0.score }
+        score = scores.reduce(0, +)
+        return score
+    }
     
 }
+
--- NewPixelsOnTheBlock/NewPixelsOnTheBlockTests/NewPixelsOnTheBlockTests.swift
+++ NewPixelsOnTheBlock/NewPixelsOnTheBlockTests/NewPixelsOnTheBlockTests.swift
@@ -9,5 +9,137 @@ import XCTest
 @testable import NewPixelsOnTheBlock
 
 class NewPixelsOnTheBlockTests: XCTestCase {
+    
+    var gameModel = GameModel()
 
+    override class func setUp() {
+        super.setUp()
+    }
+    
+    override class func tearDown() {
+        super.tearDown()
+    }
+    
+    func testPositionEquality() {
+        let pos1 = Position(x: .three, y: .three)
+        let pos2 = Position(x: .three, y: .three)
+        XCTAssertEqual(pos1, pos2)
+    }
+    
+    func testIntSubscript() {
+        let block = gameModel[0]
+        XCTAssertEqual(block?.position.x, .one)
+        XCTAssertEqual(block?.position.y, .one)
+    }
+    
+    func testPositionSubscript() {
+        let block = gameModel[Position(x: .one, y: .one)]
+        guard let index = gameModel.firstIndex(of: block) else {
+            XCTFail()
+            return
+        }
+        XCTAssertEqual(index, 0)
+    }
+    
+    func testColouredBlocks() {
+        _ = gameModel.tintBlock(at: Position(x: .three, y: .three))
+        let colouredBlocks = gameModel.colouredBlocks()
+        guard let block = colouredBlocks.first else {
+            XCTFail()
+            return
+        }
+        XCTAssertEqual(block.position.x, .three)
+        XCTAssertEqual(block.position.y, .three)
+    }
+    
+    func testWhiteBlocksWithScore() {
+        _ = gameModel.tintBlock(at: Position(x: .two, y: .one))
+        guard let block = gameModel.whiteBlocksWithScore().first else {
+            XCTFail()
+            return
+        }
+        XCTAssertEqual(block.position.x, .one)
+        XCTAssertEqual(block.position.y, .one)
+        
+        _ = gameModel.tintBlock(at: Position(x: .four, y: .two))
+        XCTAssertEqual(gameModel.whiteBlocksWithScore().count, 4)
+    }
+    
+    func testBelowAColouredBlock() {
+        let block = Block(position: Position(x: .five, y: .one))
+        XCTAssertEqual(gameModel.isBelowAColouredBlock(block), false)
+        
+        _ = gameModel.tintBlock(at: Position(x: .three, y: .one))
+        XCTAssertEqual(gameModel.isBelowAColouredBlock(Block(position: Position(x: .one, y: .one))), true)
+        XCTAssertEqual(gameModel.isBelowAColouredBlock(Block(position: Position(x: .two, y: .one))), true)
+    }
+    
+    func testAboveAColouredBlock() {
+        let block = Block(position: Position(x: .one, y: .one))
+        XCTAssertEqual(gameModel.isAboveAColouredBlock(block).0, false)
+        XCTAssertNil(gameModel.isAboveAColouredBlock(block).1)
+
+        _ = gameModel.tintBlock(at: Position(x: .one, y: .one))
+        let anotherBlock = Block(position: Position(x: .two, y: .one))
+        XCTAssertEqual(gameModel.isAboveAColouredBlock(anotherBlock).0, true)
+    }
+    
+    func testAboveANotColouredBlock() {
+        let block = gameModel.tintBlock(at: Position(x: .three, y: .four))
+        XCTAssertEqual(gameModel.isAboveANotColouredBlock(block), true)
+        
+        let anotherBlock = Block(position: Position(x: .one, y: .four))
+        XCTAssertEqual(gameModel.isAboveANotColouredBlock(anotherBlock), false)
+    }
+    
+    func testBetweenTwoColouredBLocks() {
+        let block = Block(position: Position(x: .two, y: .one))
+        XCTAssertEqual(gameModel.isBetweenTwoColouredBLocks(block), false)
+
+        _ = gameModel.tintBlock(at: Position(x: .two, y: .one))
+        _ = gameModel.tintBlock(at: Position(x: .two, y: .three))
+        XCTAssertEqual(gameModel.isBetweenTwoColouredBLocks(Block(position: Position(x: .two, y: .two))), true)
+    }
+    
+    func testTintBlock() {
+        let block = gameModel.tintBlock(at: Position(x: .one, y: .one))
+        XCTAssertEqual(block.isColoured, true)
+        XCTAssertEqual(Block(position: Position(x: .two, y: .two)).isColoured, false)
+    }
+
+    func testFinalScoreSmaller() {
+        _ = gameModel.tintBlock(at: Position(x: .one, y: .one))
+        _ = gameModel.tintBlock(at: Position(x: .one, y: .three))
+        _ = gameModel.tintBlock(at: Position(x: .one, y: .five))
+        _ = gameModel.tintBlock(at: Position(x: .two, y: .one))
+        _ = gameModel.tintBlock(at: Position(x: .two, y: .two))
+        _ = gameModel.tintBlock(at: Position(x: .two, y: .three))
+        _ = gameModel.tintBlock(at: Position(x: .two, y: .five))
+        _ = gameModel.tintBlock(at: Position(x: .three, y: .three))
+        _ = gameModel.tintBlock(at: Position(x: .three, y: .four))
+        _ = gameModel.tintBlock(at: Position(x: .three, y: .five))
+        
+        XCTAssertEqual(gameModel.isGameOver(), true)
+        
+        let finalScore = gameModel.finalScore()
+        XCTAssertEqual(finalScore, 115)
+    }
+    
+    func testFinalScoreBigger() {
+        _ = gameModel.tintBlock(at: Position(x: .one, y: .three))
+        _ = gameModel.tintBlock(at: Position(x: .one, y: .five))
+        _ = gameModel.tintBlock(at: Position(x: .two, y: .three))
+        _ = gameModel.tintBlock(at: Position(x: .two, y: .five))
+        _ = gameModel.tintBlock(at: Position(x: .three, y: .three))
+        _ = gameModel.tintBlock(at: Position(x: .three, y: .five))
+        _ = gameModel.tintBlock(at: Position(x: .four, y: .three))
+        _ = gameModel.tintBlock(at: Position(x: .four, y: .four))
+        _ = gameModel.tintBlock(at: Position(x: .four, y: .five))
+        _ = gameModel.tintBlock(at: Position(x: .five, y: .four))
+        
+        XCTAssertEqual(gameModel.isGameOver(), true)
+        
+        let finalScore = gameModel.finalScore()
+        XCTAssertEqual(finalScore, 145)
+    }
 }
