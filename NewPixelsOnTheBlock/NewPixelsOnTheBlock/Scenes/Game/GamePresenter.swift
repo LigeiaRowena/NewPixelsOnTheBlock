51054dd7d987502b61170041db469b61b02a1a4d
Francesca Corsini <ladyonirya@gmail.com>
2021-09-12 18:26:56 +0200

    Update GamePresenter.swift
    Fixed game behaviour when the user taps on a block above a coloured one or between two coloured ones

--- NewPixelsOnTheBlock/NewPixelsOnTheBlock/Scenes/Game/GamePresenter.swift
+++ NewPixelsOnTheBlock/NewPixelsOnTheBlock/Scenes/Game/GamePresenter.swift
@@ -46,22 +46,39 @@ class GamePresenter {
               let index = X.allCases.firstIndex(of: startingPosition.x) else {
             fatalError("The model of starting BlockView is nil")
         }
-        
-        // Animate the starting block and then calculate where it falls down
-        view?.presentAnimation(sender, completion: {
-            let availablePositions = Array(X.allCases.prefix(upTo: index).reversed())
-            for x in availablePositions {
-                guard let blockView = self.view?.blockViewAt(Position(x: x, y: startingPosition.y)),
-                      let position = blockView.position else {
-                    fatalError("No BlockView found")
-                }
                 
-                // Check if the animation is over
-                if (position.x == .one || self.model.isAboveAColouredBlock(position).0 || self.model.isBetweenTwoColouredBlocks(position)) {
-                    self.view?.stopAnimation(blockView)
-                    _ = self.model.setBlockColoured(at: position)
-                    break
+        // Animate the starting block
+        view?.presentAnimation(sender, completion: {
+            
+            // Check if we tapped on a block above a coloured one, if yes keep it coloured there without let it fall down
+            if self.model.isAboveAColouredBlock(startingPosition).0 {
+                self.view?.stopAnimation(sender)
+                _ = self.model.setBlockColoured(at: startingPosition)
+            // Check if we tapped on a block between two coloured ones, if yes keep it coloured there without let it fall down
+            } else if self.model.isBetweenTwoColouredBlocks(startingPosition) {
+                self.view?.stopAnimation(sender)
+                _ = self.model.setBlockColoured(at: startingPosition)
+            } else {
+                // Calculate where the block falls down
+                let availablePositions = Array(X.allCases.prefix(upTo: index).reversed())
+                for x in availablePositions {
+                    guard let blockView = self.view?.blockViewAt(Position(x: x, y: startingPosition.y)),
+                          let position = blockView.position else {
+                        fatalError("No BlockView found")
+                    }
+                    // Check if the animation is over
+                    if (position.x == .one || self.model.isAboveAColouredBlock(position).0 || self.model.isBetweenTwoColouredBlocks(position)) {
+                        self.view?.stopAnimation(blockView)
+                        _ = self.model.setBlockColoured(at: position)
+                        break
+                    }
                 }
+            }
+                        
+            // If the user tapped on position x=one just keep the block coloured there without any business other logic
+            if index == 0 {
+                self.view?.stopAnimation(sender)
+                _ = self.model.setBlockColoured(at: startingPosition)
             }
             
             // Check if game is over
